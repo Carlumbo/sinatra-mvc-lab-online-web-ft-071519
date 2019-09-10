6331c85cff5b9167e4c3ffd2cc4df326358eccef
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-09-10 21:39:55 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,6 +13,8 @@ class Piglatinizer
       word = word + w 
     elsif consonant?(word[0]) && consonant(word[1]) && consonant(word[2])
         word.slice(3..-1) + word.slice(0,3)
+    elsif consonant?(word[0]) && consonant(word[1])
+        word.slice(2..-1) + word.slice(0,2)
     elsif
   
   end 
