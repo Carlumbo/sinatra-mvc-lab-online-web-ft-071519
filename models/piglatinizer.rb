14df0ce70e8e127b47b5aa3f610ca0608cd9a97e
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-09-10 21:44:01 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -21,7 +21,7 @@ class Piglatinizer
     word << ay
   end 
    def pigifiy_sentence(sentence)
-     sentence.spilt.collect {|word| pigify_word(word)}
+     sentence.spilt.collect {|word| pigify_word(word) }.join("")
      
   
   end 
