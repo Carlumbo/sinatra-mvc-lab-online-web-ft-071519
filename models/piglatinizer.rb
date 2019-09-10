e1f75e2bad327407da345b6b15e595a70fc75911
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-09-10 21:51:25 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -15,9 +15,9 @@ class PigLatinizer
     
     if !consonant?(word[0])
       word = word + w 
-    elsif consonant?(word[0]) && consonant(word[1]) && consonant(word[2])
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
         word.slice(3..-1) + word.slice(0,3)
-    elsif consonant?(word[0]) && consonant(word[1])
+    elsif consonant?(word[0]) && consonant?(word[1])
         word.slice(2..-1) + word.slice(0,2)
     else
       word = word.slice(1..-1) + word.slice(0,1)
